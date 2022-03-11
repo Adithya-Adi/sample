bae33df61353658bd0176edfbc9f927f267a94d6
Adithya <adithyahebbar32@gmail.com>
2022-03-11 16:01:43 +0530

    changed

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { process_params } = require('express/lib/router');
 const router = express.Router()
 const {Posts} = require('../models/Post');
 const {User} = require('../models/user');
@@ -15,5 +16,23 @@ router.post('/', async(req,res)=>{
     const result = await posts.save()
     res.send(result)
 });
+
+router.delete('/:title', async(req,res)=>{
+    try{
+    const deleted = await Posts.remove({title: req.params.title})
+    res.send(deleted)
+    }catch(err){
+        res.send(err);
+    }
+})
+
+router.patch('/:title', async(req,res)=>{
+    try{
+    const updatedPost = await Posts.updateOne({title: req.params.title}, {$set: {title: req.body.title}})
+    res.json(updatedPost)
+}catch(err){
+        res.send(err)
+    }
+})
 
 module.exports = router
